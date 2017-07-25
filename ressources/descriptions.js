8041c261c06e333530b5bcbcb792cb91d54e7b96
Gibs13 <gsgwlol@gmail.com>
2017-07-25 14:04:24 +0200

    v1.1.1

---
+++ ressources/descriptions.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+    'CLOUDSTER': 'Description cloudster',
+    'CLOUDCRUISER': '',
+    'CLOUDSURFER': '',
+    'CLOUDRUSH': '',
+    'CLOUD': '',
+    'CLOUDFLYER': '',
+    'CLOUDFLOW': '',
+    'CLOUDFLASH': '',
+    'CLOUDVENTURE MIDTOP': '',
+    'CLOUDVENTURE': '',
+    'CLOUDVENTURE PEAK': '',
+    'CLOUDVENTURE WATERPROOF': ''
+}
